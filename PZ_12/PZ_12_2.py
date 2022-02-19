ee05d99a52353af9f79d2bc5c9ef3e8243eb968d
Николай <92692339+litvinkokolya@users.noreply.github.com>
2022-02-19 20:58:27 +0300

    Update PZ_12_2.py

--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -1,18 +1,25 @@
-# Разработать программу с применением пакета tk, взяв в качестве условия задачу из ПЗ 2-8.
-# Я взял ПЗ #2.
+# Разработать программу с применением пакета tk, взяв в качестве условия задачу из ПЗ 3-8.
+# Я взял ПЗ #4.1:
+# Дано целое число N (>0). Найти значение выражения 1.1 - 1.2 +1.3 - ...
+# (N слагаемых, знаки чередуются). Условный оператор не использовать.
 import tkinter as tk
-import math
 
 def func():
-    try:
-        S = float(entry_1.get())
-        if 9 < S < 100:
-            Res1 = (S // 10) + math.fmod(S, 10)
-            Res2 = (S // 10) * math.fmod(S, 10)
-        label.config(text="Сумма и произведение его цифр будет равна: {}, {}".format(Res1, Res2))
-    except ValueError:
-        label.config(text="Ошибка, введите цифры")
-
+    N = int(entry_1.get())
+    while type(N) != int:
+        try:
+            N = int(N)
+        except ValueError:
+            print("Введите число!")
+            N = input("Введите число")
+    S = 0.0
+    i = 1
+    while N >= 1:
+        x = (1 + i * 0.1) * (-1) ** (i + 1)
+        S += x
+        N -= 1
+        i += 1
+    label.config(text="Сумма: {}".format(S))
 
 root = tk.Tk()
 entry_1 = tk.Entry(root)
